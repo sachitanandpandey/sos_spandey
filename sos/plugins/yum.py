9982f510e40cc66128e7c7ef5e54da36d3bb4c3a
spandey <spandey@redhat.com>
2014-11-12 15:30:47 +0530

    [Yum] Removed identity
     Removed identity

--- sos/plugins/yum.py
+++ sos/plugins/yum.py
@@ -52,7 +52,6 @@ class Yum(Plugin, RedHatPlugin):
         self.add_cmd_output([
             "subscription-manager list --installed",
             "subscription-manager list --consumed",
-            "subscription-manager identity"
         ])
         self.add_cmd_output("rhsm-debug system --sos --no-archive "
                             "--no-subscriptions --destination %s"
